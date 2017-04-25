1ac29a235ab7f903ee14b3400709f9e34bab4b63
LomotinKonstantin <ke.lomotin@gmail.com>
2017-04-25 22:15:04 +0300

    Добавлена полная документация к интерфейсу ссобщений
    Signed-off-by: LomotinKonstantin <ke.lomotin@gmail.com>

--- src/MessageInterface.java
+++ src/MessageInterface.java
@@ -1,5 +1,6 @@
 import javafx.util.Pair;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 /**
  * Created by Konstantin on 25.04.2017.
@@ -32,17 +33,35 @@ public interface MessageInterface
     Stack<Pair<String, Stack<Object>>> stackOfFunctions();
 
     /**
-     * Метод доступа к значению указателя CC.
+     * Метод доступа к значению указателя <b>CC</b>.
      * @return Возвращает одно из значений {@link MessageInterface#RIGHT} или
      * {@link MessageInterface#LEFT}
      */
     int cc();
 
     /**
-     * Метод доступа к значению указателя DP.
+     * Метод доступа к значению указателя <b>DP</b>.
      * @return Возвращает одно из значений {@link MessageInterface#TOP},
      * {@link MessageInterface#RIGHT}, {@link MessageInterface#BOTTOM} или
      * {@link MessageInterface#LEFT}
      */
     int dp();
+
+    /**
+     * Метод доступа к координате <b>X</b> агента (номер пикселя с верхнего левого угла).
+     * @return Текущая координата X агента.
+     */
+    int x();
+
+    /**
+     * Метод доступа к координате <b>Y</b> агента (номер пикселя с верхнего левого угла).
+     * @return Текущая координата Y агента.
+     */
+    int y();
+
+    /**
+     * Метод доступа к режиму работы агента.
+     * @return режим работы агента.
+     */
+    String mode();
 }
